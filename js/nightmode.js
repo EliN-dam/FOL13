c4dc7bffbb358a286c3fbeea5b73267edd810694
Elí Nahum Cairós Rodríguez <elimasterpc@gmail.com>
2020-03-10 16:58:17 +0000

    Footer del Index,  estructuras en als otras páginas y empezar a maquetar la paginas de protocolos.

--- js/nightmode.js
+++ js/nightmode.js
@@ -1,5 +1,7 @@
-const nightMode = document.getElementById('night-mode');
+const nightMode = document.getElementById('night-mode'),
+      bgLayer = document.getElementById('bg-layer');
 
 nightMode.addEventListener('click', function() {
     nightMode.classList.toggle('active');
+    bgLayer.classList.toggle('active');
 })
