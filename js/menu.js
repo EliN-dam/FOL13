8c55a10bd53c4bcd2464f6860d4de0890087f7d8
Elí Nahum Cairós Rodríguez <elimasterpc@gmail.com>
2020-03-19 01:32:42 +0000

    Header, footer y slider a 768p, animar el menu hamburgesa.

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,22 @@
 const menu = document.querySelector('.menu'),
-      nav = document.querySelector('nav');
+      nav = document.querySelector('nav')
+      navLinks = nav.querySelectorAll('ul li');
 let menuOpen = false;
 
 menu.addEventListener('click', function() {
     if (menuOpen) {
         menu.classList.remove('open');
         nav.classList.remove('open');
+        navLinks.forEach(link => {
+            link.style.animation = "";
+        });
         menuOpen = false
     } else {
         menu.classList.add('open');
         nav.classList.add('open');
+        navLinks.forEach((link, index) => {
+            link.style.animation = `showLinkUp .5s ease ${index / 3 + .5}s forwards`;
+        });
         menuOpen = true;
     }
 });
